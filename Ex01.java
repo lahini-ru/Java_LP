6a884ec687f347acb4b3b3d6546e9988952f4407
lahini-ru <58974696+lahini-ru@users.noreply.github.com>
2020-01-06 10:28:39 +0530

    Add files via upload

---
+++ Ex01.java
@@ -0,0 +1,17 @@
+public class Ex01{
+
+	public static void main(String []args){
+
+	int num=214536;
+	int temp=0;
+	
+	while(num!=0){
+	
+		temp=temp*10;
+		temp=temp+num%10;
+		num=num/10;
+	}
+
+	System.out.print(temp);
+	}
+}
