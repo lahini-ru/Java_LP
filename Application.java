0c2dfdb51aebc9f8d28ec6d22254990e3c6f6c29
lahini-ru <58974696+lahini-ru@users.noreply.github.com>
2020-01-15 22:56:28 +0530

    Add files via upload

--- Application.java
+++ Application.java
@@ -1,14 +1,29 @@
-package Test;
+package EncTest;
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Application {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		UpperCase upperCase=new UpperCase();
-		upperCase.printer();
+		EncapsulationEx obj=new EncapsulationEx();
+
+		System.out.println("Enter the account number");
+		Scanner sc=new Scanner(System.in);
+		obj.setAccountNumber(sc.nextInt());
+		
+		System.out.println("Enter the bank balance");
+		obj.setBankBalance(sc.nextDouble());
+		
+		System.out.println("Enter the debit amount");
+		obj.setDebitAmount(sc.nextDouble());
+		
+		double bBal=(double)obj.getBankBalance();
+		double dBal=(double)obj.getDebitAmount();
+		double remainBal=bBal-dBal;
+		System.out.println("Your remaining balance is "+remainBal);
+		
 	}
 
 }
