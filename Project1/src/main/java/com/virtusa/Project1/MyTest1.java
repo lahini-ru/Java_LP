7f689e5c8b545069cdb54f8fae85d3235839adcd
Lahini <58974696+lahini-ru@users.noreply.github.com>
2020-01-19 22:39:01 +0530

    Add files via upload

---
+++ Project1/src/main/java/com/virtusa/Project1/MyTest1.java
@@ -0,0 +1,16 @@
+package com.virtusa.Project1;
+
+public class MyTest1 {
+
+	public static void main(String[] args) {
+
+		sayHello();
+
+	}
+	
+	public static void sayHello() {
+		
+		System.out.println( "MyTest1 says Hello!" );
+	}
+
+}
---
+++ Project2/src/main/java/com/virtusa/Project2/MyTest2.java
@@ -0,0 +1,17 @@
+package com.virtusa.Project2;
+
+public class MyTest2 {
+
+	public static void main(String[] args) {
+
+		sayHello();
+		MyTest1.sayHello();
+
+	}
+	
+	public static void sayHello() {
+		
+		System.out.println( "MyTest2 says Hello!" );
+	}
+
+}
